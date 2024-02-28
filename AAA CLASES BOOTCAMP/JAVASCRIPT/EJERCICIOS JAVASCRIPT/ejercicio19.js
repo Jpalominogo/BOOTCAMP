0c8915050879eb97920cd7497eace9bf080a7243
José Palomino <josefra.palomino@gmail.com>
2024-02-28 17:46:40 +0100

    cambios en ejercicio 18 y 19

---
+++ AAA CLASES BOOTCAMP/JAVASCRIPT/EJERCICIOS JAVASCRIPT/ejercicio19.js
@@ -0,0 +1,19 @@
+/* 
+Mixed For e includes: Usa un bucle for para recorrer todos los juguetes y elimina los que incluyan la palabra gato. Recuerda que puedes usar la función .includes() para comprobarlo. Puedes usar este array:
+*/
+
+const toys = [
+    { id: 5, name: 'Buzz MyYear' },
+    { id: 11, name: 'Action Woman' },
+    { id: 23, name: 'Barbie Man' },
+    { id: 40, name: 'El gato con Guantes' },
+    { id: 42, name: 'El gato felix' }
+]
+
+for (let i = 0; i < toys.length; i++) {
+    if (toys[i].name.includes('gato')) {
+        toys.splice(i, 1)
+    } else {
+        console.log(`La id es: ${toys[i].id} y el nombre es: ${toys[i].name}`)
+    }
+}
