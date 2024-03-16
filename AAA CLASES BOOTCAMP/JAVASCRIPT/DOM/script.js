620a31d8718db1f5b778f3bb475014e66e3ec496
José Palomino <josefra.palomino@gmail.com>
2024-03-16 15:52:21 +0100

    update

--- AAA CLASES BOOTCAMP/JAVASCRIPT/DOM/script.js
+++ AAA CLASES BOOTCAMP/JAVASCRIPT/DOM/script.js
@@ -5,14 +5,16 @@ title.innerText = 'Title'
 console.log(title)
 
 document.body.appendChild(title)
+
 const imagen = document.createElement('img')
+
 imagen.src = 'https://ethic.es/wp-content/uploads/2023/03/imagen.jpg'
 imagen.alt = 'imagen random'
 
 document.body.appendChild(imagen)
 
 const div = document.createElement('div')
-
+const content = 'Este es el contenido'
 div.innerHTML = `<p>Soy un párrafo</p>`
 
 document.body.appendChild(div)
